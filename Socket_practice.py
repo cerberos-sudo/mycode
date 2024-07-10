04e76d8fd5ba232c093d96fc4e29b51bc368d01e
cerberos-sudo <brodwolfbentley@gmail.com>
2024-07-10 13:13:30 -0700

    Update Socket_practice.py

--- Socket_practice.py
+++ Socket_practice.py
@@ -24,6 +24,32 @@ def server_program():
         data = input(' -> ')
         conn.send(data.encode())  # send data to the client
 
+    def play_game(client_socket):
+    options = ['rock', 'paper', 'scissors']
+    print(client_choice)    
+    while True:
+        # Receive player's choice
+        client_choice = client_socket.recv(1024).decode().strip().lower()
+
+    if client_choice not in options:
+        client_socket.send("Invalid choice. Please choose rock, paper, or scissors.\n".encode())
+        
+
+        # Server's choice
+    import random
+    server_choice = random.choice(options)
+    client_socket.send(f"Server chooses: {server_choice}\n".encode())
+
+        # Determine winner
+    if client_choice == server_choice:
+        client_socket.send("It's a tie!\n".encode())
+    elif (client_choice == 'rock' and server_choice == 'scissors') or \
+        (client_choice == 'paper' and server_choice == 'rock') or \
+        (client_choice == 'scissors' and server_choice == 'paper'):
+        client_socket.send("You win!\n".encode())
+    else:
+        client_socket.send("Server wins!\n".encode())
+   
     conn.close()  # close the connection
 
 
