a6549de46ffa99cededfd91068b35ad353fe2acd
cerberos-sudo <brodwolfbentley@gmail.com>
2024-07-15 09:09:40 -0700

    Update SocketClient_Practice.py

--- SocketClient_Practice.py
+++ SocketClient_Practice.py
@@ -8,7 +8,8 @@ def client_program():
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
+   # message = input(" -> ")  # take input
+    message = ""
 
     while message.lower().strip() != 'bye':
         client_socket.send(message.encode())  # send message
